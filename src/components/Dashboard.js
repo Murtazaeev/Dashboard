8d697a03741c06dfbf6ad56abeb62a17ef77f7c0
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-02-19 18:26:53 +0100

    styling added to table

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,13 +7,36 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import { Grid } from '@material-ui/core';
 
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
+import { Typography } from '@material-ui/core';
 
-const styles = makeStyles({
+const styles = (theme) => ({
 	table: {
-		minWidth: 650
+		minWidth: 0
+	},
+	TableContainer: {
+		padding: '40px 40px'
+	},
+	button: {
+		width: 95
+	},
+	editButton: {
+		background: theme.palette.warning.main,
+		color: 'white'
+	},
+	tableHeader: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		padding: '30px 20px'
+	},
+	typography: {
+		fontWeight: 750
 	}
 });
 
@@ -21,36 +44,81 @@ class Dashboard extends React.Component {
 	componentDidMount() {
 		this.props.fetchUsers();
 	}
+
+	getRows = () => {
+		return (
+			<TableRow>
+				<TableCell>Id</TableCell>
+				<TableCell align="right">Name</TableCell>
+				<TableCell align="right">Username</TableCell>
+				<TableCell align="right">Email</TableCell>
+				<TableCell align="right">City</TableCell>
+				<TableCell align="right">Edit</TableCell>
+				<TableCell align="right">Delete</TableCell>
+			</TableRow>
+		);
+	};
+
 	render() {
 		const { classes } = this.props;
-		console.log(this.props.users);
 		return (
-			<TableContainer component={Paper}>
-				<Table className={classes.table} aria-label="simple table">
-					<TableHead>
-						<TableRow>
-							<TableCell>Id</TableCell>
-							<TableCell align="right">Name</TableCell>
-							<TableCell align="right">Username</TableCell>
-							<TableCell align="right">Email</TableCell>
-							<TableCell align="right">City</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{this.props.users.map((user) => (
-							<TableRow key={user.name}>
-								<TableCell component="th" scope="row">
-									{user.id}
-								</TableCell>
-								<TableCell align="right">{user.name}</TableCell>
-								<TableCell align="right">{user.username}</TableCell>
-								<TableCell align="right">{user.email}</TableCell>
-								<TableCell align="right">{user.address.city}</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
+			<div>
+				<Typography className={classes.typography} variant="h3">
+					Dashboard
+				</Typography>
+				<div className={classes.TableContainer}>
+					<Paper className={classes.tableHeader}>
+						<Typography className={classes.typography} variant="h4">
+							User list
+						</Typography>
+						<Button variant="contained" color="primary" size="large">
+							Add new
+						</Button>
+					</Paper>
+					<TableContainer component={Paper}>
+						<Table className={classes.table} aria-label="simple table">
+							<TableHead>{this.getRows()}</TableHead>
+							<TableBody>
+								{this.props.users.map((user) => (
+									<TableRow key={user.name}>
+										<TableCell component="th" scope="row">
+											{user.id}
+										</TableCell>
+										<TableCell align="right">{user.name}</TableCell>
+										<TableCell align="right">{user.username}</TableCell>
+										<TableCell align="right">{user.email}</TableCell>
+										<TableCell align="right">{user.address.city}</TableCell>
+										<TableCell align="right">
+											{
+												<Button
+													className={classes.editButton}
+													variant="contained"
+													color="primary"
+													startIcon={<EditIcon />}
+												>
+													edit
+												</Button>
+											}
+										</TableCell>
+										<TableCell align="right">
+											{
+												<Button
+													className={classes.button}
+													variant="contained"
+													color="secondary"
+													startIcon={<DeleteIcon />}
+												>
+													delete
+												</Button>
+											}
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</div>
+			</div>
 		);
 	}
 }
@@ -59,5 +127,4 @@ const mapStateToProps = (state) => {
 	return { users: state.users };
 };
 
-const styleWrapperDashboard = withStyles(styles, { withTheme: true })(Dashboard);
-export default connect(mapStateToProps, { fetchUsers })(styleWrapperDashboard);
+export default connect(mapStateToProps, { fetchUsers })(withStyles(styles)(Dashboard));
